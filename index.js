6569c5a41358322c02632af117d2d808ebfba615
dann <austindan168@gmail.com>
2024-10-17 20:49:17 +0300

    index.js

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
+
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then((resp) => resp.json()) 
+    .then(data => {
+      renderBooks(data)
+    })
+    .catch(error => console.log(error))
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
+  const list = document.getElementById('book-list')
   books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+    const listItem = document.createElement('li');
+    listItem.textContent = book.name
+    list.appendChild(listItem)
+  })
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+document.addEventListener('DOMContentLoaded', () => {
+  fetchBooks()
+})
